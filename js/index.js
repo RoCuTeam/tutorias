ce29f897885c99082c2ca9ba5ea14d7c19e0dfde
Cuauhtli Fernando <cuauh_fer_bh@hotmail.com>
2019-09-16 12:51:07 -0500

    ResetPassword y home

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ function login (){
                       "<input class='btn-data' type='text' placeholder='&#128272; Usuario' name='usuario'>"+
                       "<input class='btn-data' type='password' placeholder='&#128272; Contraseña' name='clave'>"+
                       "<a onClick='menu()'><input class='btn-submit' type='submit' value='Ingresar'></a>"+
-                      "<a class='misspass' href='#'>¿Olvidaste tu contraseña?</a>"+
+                      "<a class='misspass' href='resetPassword.html'>¿Olvidaste tu contraseña?</a>"+
                     "</div>";
 
   document.getElementById("over-content").innerHTML = formulario;
